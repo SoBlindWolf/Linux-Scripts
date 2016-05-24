15817f0bb39f07abef6b343d63e89746a5931ccb
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-05-24 17:47:01 -0500

    Added PHP7.0 packages

--- auto-install.sh
+++ auto-install.sh
@@ -1,15 +1,15 @@
 while true; do
     read -p "Do you wish to install main packages? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install screen build-essential curl openssh-server apache2 mysql-server php7.0 php7.0-mcrypt php7.0-bz2 -y; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get purge php5-common -y && sudo apt-get remove php5-common -y && sudo apt-get install screen wget apache2 apache2-utils python-software-properties php7.0 php7.0-mcrypt php7.0-bz2 libapache2-mod-php7.0 build-essential libgd2-xpm-dev curl openssh-server mysql-server -y && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 while true; do
     read -p "Do you wish to install opt packages? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo apt-get update && sudo apt-get install make git svn -y && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
+        [Yy]* ) sudo apt-get update && sudo apt-get install make git subversion -y && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
