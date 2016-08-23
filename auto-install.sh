3b15be69a427d04ea08f863272d20ea01b0e0cb9
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-08-23 09:19:34 -0500

    Update auto-install.sh
    Added Apache2 Restart to enable modules.

--- auto-install.sh
+++ auto-install.sh
@@ -1,7 +1,7 @@
 while true; do
     read -p "Do you wish to install main packages? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get purge php5-common -y && sudo apt-get remove php5-common -y && sudo apt-get install screen wget apache2 apache2-utils python-software-properties php7.0 php7.0-mcrypt php7.0-bz2 mysql-client php7.0-fpm  php7.0-curl libapache2-mod-php7.0 build-essential libgd2-xpm-dev curl openssh-server mysql-server openssl -y && sudo a2enmod proxy_fcgi setenvif && sudo a2enconf php7.0-fpm && sudo service php7.0-fpm restart && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
+        [Yy]* ) sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get purge php5-common -y && sudo apt-get remove php5-common -y && sudo apt-get install screen wget apache2 apache2-utils python-software-properties php7.0 php7.0-mcrypt php7.0-bz2 mysql-client php7.0-fpm  php7.0-curl libapache2-mod-php7.0 build-essential libgd2-xpm-dev curl openssh-server mysql-server openssl -y && sudo a2enmod proxy_fcgi setenvif && sudo a2enconf php7.0-fpm && sudo service php7.0-fpm restart && sudo service apache2 restart && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
