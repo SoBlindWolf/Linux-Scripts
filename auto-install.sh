d15c9007194ad789208815e5f77e6b688e8be4b6
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-05 13:01:10 -0500

    Added more PHP7 packages

--- auto-install.sh
+++ auto-install.sh
@@ -1,7 +1,7 @@
 while true; do
     read -p "Do you wish to install main packages? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get purge php5-common -y && sudo apt-get remove php5-common -y && sudo apt-get install screen wget apache2 apache2-utils python-software-properties php7.0 php7.0-mcrypt php7.0-bz2 mysql-client php7.0-curl libapache2-mod-php7.0 build-essential libgd2-xpm-dev curl openssh-server mysql-server php7.0-mysql openssl -y && sudo service apache2 restart && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
+        [Yy]* ) sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get purge php5-common -y && sudo apt-get remove php5-common -y && sudo apt-get install screen wget apache2 apache2-utils python-software-properties mysql-server mysql-client build-essential libgd2-xpm-dev curl tar unzip git openssh-server openssl php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-mcrypt php7.0-bz2 php7.0-curl php7.0-gd php7.0-pdo php7.0-mbstring php7.0-tokenizer php7.0-bcmath php7.0-xml php7.0-mysql -y && sudo service apache2 restart && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -9,7 +9,7 @@ done
 while true; do
     read -p "Do you wish to install opt packages? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo apt-get update && sudo apt-get install make git subversion -y && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
+        [Yy]* ) sudo apt-get update && sudo apt-get install make subversion -y && sudo apt-get autoremove -y && sudo apt-get clean -y; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
