2ae7b80ace3476fdfd6ad64c663caeadcabcf947
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-19 15:49:25 -0500

    Update auto-install-mozilla-extras.sh

--- auto-install-mozilla-extras.sh
+++ auto-install-mozilla-extras.sh
@@ -1,7 +1,7 @@
 while true; do
     read -p "Would you like to install AdBlock Plus? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo cd /usr/local/sbin/mozilla-extension-manager && sudo mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/adblock-plus/addon-1865-latest.xpi; break;;
+        [Yy]* ) sudo /usr/local/sbin/mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/adblock-plus/addon-1865-latest.xpi; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -10,7 +10,7 @@ done
 while true; do
     read -p "Would you like to install Dark Background and Light Text? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo cd /usr/local/sbin/mozilla-extension-manager && sudo mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/dark-background-light-text/addon-497798-latest.xpi; break;;
+        [Yy]* ) sudo /usr/local/sbin/mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/dark-background-light-text/addon-497798-latest.xpi; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -19,7 +19,7 @@ done
 while true; do
     read -p "Would you like to install FT Deep Dark? [Y/n]" yn
     case $yn in
-        [Yy]* ) sudo cd /usr/local/sbin/mozilla-extension-manager && sudo mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/ft-deepdark/platform:5/addon-295337-latest.xpi; break;;
+        [Yy]* ) sudo /usr/local/sbin/mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/ft-deepdark/platform:5/addon-295337-latest.xpi; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
