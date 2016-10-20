256174d266977753bafb96979349d36a7ddd61ed
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-19 20:03:44 -0500

    Update auto-install-mozilla-extras.sh

--- auto-install-mozilla-extras.sh
+++ auto-install-mozilla-extras.sh
@@ -1,4 +1,13 @@
 while true; do
+    read -p "Would you like to install Startpage HTTPS Privacy Search Engine? [Y/n]" yn
+    case $yn in
+        [Yy]* ) sudo /usr/local/sbin/mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/startpage-https-privacy-search/addon-13036-latest.xml; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
     read -p "Would you like to install AdBlock Plus? [Y/n]" yn
     case $yn in
         [Yy]* ) sudo /usr/local/sbin/mozilla-extension-manager --install --global https://addons.mozilla.org/firefox/downloads/latest/adblock-plus/addon-1865-latest.xpi; break;;
