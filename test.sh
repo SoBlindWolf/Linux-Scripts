85539137c2d4a9477c2d29bb42e3ad50f105db5b
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-26 10:12:57 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
+a2sa "/etc/apache2/sites-available/"
+webdir "/var/www/html/clients/"
+
 echo "This will create a new VirtualHost file..."
 read -p "Client ID: " client
 read -p "Main Domain: " domain
 read -p "Email User: " email
-#read -p "Directory: " dir
-echo "This will create a user with ID $client with $domain with the email $email@$domain and their website will be located in /var/www/html/clients/$client/"
+echo "This will make a vhost with Client as $client with $domain using the email address $email@$domain with website located in $webdir/$client/"
+
