871c70b682bfe81a75564071a8a2756e24bd4100
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-26 10:59:51 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 a2sa="/etc/apache2/sites-available/"
 webdir="/var/www/html/clients"
+ddom="example.domain"
+ddem="webmaster@example.domain"
+ddir="/var/www/html/clients/example"
 
 echo "This will create a new VirtualHost file..."
 read -p "Client ID: " client
-read -p "Main Domain: " domain
-read -p "Email User: " email
+read -p "Main Domain: " cdom
+read -p "Email User: " cdem
 echo "This will make a vhost with Client as $client with $domain using the email address $email@$domain with website located in $webdir/$client/"
-
+cdir="$webdir/$client/"
+cd $a2sa && sudo cp 00-$ddom.conf 00-$cdom.conf
+sudo sed -i "s/$ddom/$cdom/g" 00-$cdom.conf
+sudo sed -i "s/$ddem/$cdem/g" 00-$cdom.conf
+sudo sed -i "s/$ddir/$cdir/g" 00-$cdom.conf
