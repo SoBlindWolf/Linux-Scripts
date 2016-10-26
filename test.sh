e8f67e76c218fd8d7b97952e886f8c7435042310
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-26 11:38:06 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,10 +9,12 @@ read -p "Client ID: " client
 read -p "Main Domain: " cdom
 read -p "Email User: " cdem
 echo "This will make a vhost with Client as $client with $domain using the email address $email@$domain with website located in $webdir/$client/"
+sudo mkdir $webdir
 cdir="$webdir/$client"
 #cdir="($webdir)/($client)"
 cd $a2sa && sudo cp 000-$ddom.conf 000-$cdom.conf
 sudo sed -i "s/$ddom/$cdom/g" 000-$cdom.conf
 sudo sed -i "s/$ddem/$cdem/g" 000-$cdom.conf
 sudo sed -i "s/$ddir/$cdir/g" 000-$cdom.conf
+sudo mv $ddir $cdir
 done
