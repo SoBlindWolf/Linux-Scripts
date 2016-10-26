23a71f9483b652068f10f62daa0ac45cead88587
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-26 09:41:47 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "This will create a new VirtualHost file..."
-read -p "Client: " client
-read -p "Domain: " domain
-read -p "Email: " email
-read -p "Directory: " dir
-echo "This will create a new VirtualHost for client '$client' with domain as '$domain' in '$dir'"
+read -p "Client ID: " client
+read -p "Main Domain: " domain
+read -p "Email User: " email
+#read -p "Directory: " dir
+echo "This will create a user with ID $client with $domain with the email $email@$domain and their website will be located in /var/www/html/clients/$client/"
