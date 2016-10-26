c210aee13681135693a3e475aa169363c56f4c5b
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-10-26 13:53:45 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,11 +9,26 @@ read -p "Client ID: " cli
 read -p "Main Domain: " cdom
 read -p "Email User: " cdem
 echo "This will make a vhost with Client as $cli with $cdom using the email address $cdem@$cdom with website located in $webdir/$cli/"
-sudo mkdir $ddir
 cdir="$webdir/$cli"
 ccem="$cdem@$cdom"
-cd $a2sa && sudo cp 000-$ddom.conf 000-$cdom.conf
-sudo sed -i "s/$ddem/$ccem/g" 000-$cdom.conf
-sudo sed -i "s/$ddom/$cdom/g" 000-$cdom.conf
-sudo sed -i "s|$webdir|$cdir|g" 000-$cdom.conf
-sudo mv $ddir $cdir
+
+while true; do
+    read -p "Is this information correct? [Y/n]" yn
+    case $yn in
+        [Yy]* ) sudo mkdir $ddir 
+		&& cd $a2sa 
+		&& sudo cp 000-$ddom.conf 000-$cdom.conf 
+		&& sudo sed -i "s/$ddem/$ccem/g" 000-$cdom.conf 
+		&& sudo sed -i "s/$ddom/$cdom/g" 000-$cdom.conf 
+		&& sudo sed -i "s|$webdir|$cdir|g" 000-$cdom.conf 
+		&& sudo mv $ddir $cdir; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+#sudo mkdir $ddir
+#cd $a2sa && sudo cp 000-$ddom.conf 000-$cdom.conf
+#sudo sed -i "s/$ddem/$ccem/g" 000-$cdom.conf
+#sudo sed -i "s/$ddom/$cdom/g" 000-$cdom.conf
+#sudo sed -i "s|$webdir|$cdir|g" 000-$cdom.conf
+#sudo mv $ddir $cdir
